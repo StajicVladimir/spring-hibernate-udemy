0adf9e7cb48f23d307a5cfe24acf3aa3b7d0beb0
stajicvladimir <vstajic@gmail.com>
2018-09-28 14:54:57 +0200

    WTF

--- src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
+++ src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -4,43 +4,39 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class DeleteStudentDemo {
+public class GetInstructorDetailDemo {
 
 	public static void main(String[] args) {
 		SessionFactory factory = new Configuration()
 										.configure("hibernate.cfg.xml")
-										.addAnnotatedClass(Student.class)
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
 										.buildSessionFactory();
 		
 		Session session = factory.getCurrentSession();
 		
 		try {
 			
-			int studentId = 1;
-			
-			session = factory.getCurrentSession();
 			session.beginTransaction();
 			
-			System.out.println("\nGetting student with id: " + studentId);
+			int theId = 2999;
+			InstructorDetail tempInstructorDetail = 
+					session.get(InstructorDetail.class, theId);
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
 			
-			Student myStudent = session.get(Student.class, studentId);
-			
-			System.out.println("Get complete: " + myStudent);
-			
-			System.out.println("Deleting student!");
-//			session.delete(myStudent);
-			
-			session.createQuery("delete from Student where id=2")
-				.executeUpdate();
-			
+			System.out.println("the associated instructor: " + 
+					tempInstructorDetail.getInstructor());
 			session.getTransaction().commit();
 			System.out.println("Done!");
 			
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		} finally {
+			session.close();
 			factory.close();
 		}
 	}
